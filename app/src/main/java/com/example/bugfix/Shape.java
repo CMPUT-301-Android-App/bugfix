f7560bce5659361214c66dc6802c075162eaae60
Katharine Zhong <113125912+katzhong@users.noreply.github.com>
2024-09-25 23:56:23 -0600

    add color string field

--- app/src/main/java/com/example/bugfix/Shape.java
+++ app/src/main/java/com/example/bugfix/Shape.java
@@ -2,6 +2,7 @@ package com.example.bugfix;
 
 public abstract class Shape {
     private int x, y;
+    private string color = "green";
 
     public int getX() {
         return x;
@@ -11,11 +12,19 @@ public abstract class Shape {
         return y;
     }
 
+    public string getColor() {
+        return color;
+    }
+
     public void setX(int x) {
         this.x = x;
     }
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public void setColor(string color) {
+        this.color = color
     }
 }
