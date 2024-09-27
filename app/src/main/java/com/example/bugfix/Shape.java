c3b61a0cbdc0b4601b2130cb6d8976e78016d1e7
avreet-14 <157077589+avreet-14@users.noreply.github.com>
2024-09-27 15:15:58 -0600

    Update Shape.java
    changed the color blue

--- app/src/main/java/com/example/bugfix/Shape.java
+++ app/src/main/java/com/example/bugfix/Shape.java
@@ -2,7 +2,7 @@ package com.example.bugfix;
 
 public abstract class Shape {
     private int x, y;
-    String color = "bone";
+    String color = "blue";
 
     public int getX() {
         return x;
